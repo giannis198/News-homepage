25a6b8778421cc2dd6849740b4d321f5a23dbb9d
giannis198 <john_nik@windowslive.com>
2024-01-08 21:42:27 +0200

    refactor:section3

--- components/Section3.tsx
+++ components/Section3.tsx
@@ -2,30 +2,40 @@ import Image from "next/image";
 import Box from "./Box";
 
 const Section3 = () => {
+  const boxContent = [
+    {
+      index: "01",
+      title: "Reviving Retro PCs",
+      description: "What happens when old PCs are given modern upgrades?",
+      imageUrl: "/image-retro-pcs.jpg",
+    },
+    {
+      index: "02",
+      title: "Our best picks for various needs and budgets.",
+      description: "Top 10 Laptops of 2022",
+      imageUrl: "/image-top-laptops.jpg",
+    },
+    {
+      index: "03",
+      title: "How the pandemic has sparked fresh opportunities.",
+      description: "The Growth of Gaming",
+      imageUrl: "/image-gaming-growth.jpg",
+    },
+  ];
   return (
     <section
       className="grid-cols-3 items-start  gap-x-12 space-y-5
     pb-10 lg:grid lg:space-y-0"
     >
-      <Box
-        imgUrl="/image-retro-pcs.jpg"
-        title="Reviving Retro PCs"
-        description="What happens when old PCs are given modern upgrades?"
-        index="01"
-      />
-      <Box
-        index="02"
-        description="Our best picks for various needs and budgets."
-        title="Top 10 Laptops of 2022"
-        imgUrl="/image-top-laptops.jpg"
-      />
-
-      <Box
-        index="03"
-        description="How the pandemic has sparked fresh opportunities."
-        title="The Growth of Gaming"
-        imgUrl="/image-gaming-growth.jpg"
-      />
+      {boxContent.map((content) => (
+        <Box
+          key={content.index}
+          title={content.title}
+          description={content.description}
+          imgUrl={content.imageUrl}
+          index={content.index}
+        />
+      ))}
     </section>
   );
 };
